5faa3e232c29d81267d00bc665ebb9398190ad89
Josh <joshuadanielp@gmail.com>
2020-07-21 11:01:28 -0500

    7/21 update

--- Instructions/static/js/logic.js
+++ Instructions/static/js/logic.js
@@ -34,25 +34,30 @@
 //     }).addTo(myMap);
   
 //   });
-  
+
 function createMap(earthquakes) {
 
     // Create the tile layer that will be the background of our map
-    // var lightmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-    //     attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-    //     maxZoom: 18,
-    //     id: "light-v10",
-    //     accessToken: API_KEY
-    // });
-  
-    var lightmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-            attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
-            tileSize: 512,
-            maxZoom: 18,
-            zoomOffset: -1,
-            id: "mapbox/streets-v11",
-            accessToken: API_KEY
-        }).addTo(lightmap);
+ 
+    // var lightmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    //         attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+    //         tileSize: 512,
+    //         maxZoom: 18,
+    //         zoomOffset: -1,
+    //         id: "mapbox/streets-v11",
+    //         accessToken: API_KEY
+    //     });
+
+
+    //FIX THIS VVV //
+    var lightmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+      attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+      maxZoom: 18,
+      id: "light-v10",
+      accessToken: API_KEY
+    });
+      
+    // var earthquakes = new L.LayerGroup()
 
     // Create a baseMaps object to hold the lightmap layer
     var baseMaps = {
@@ -67,18 +72,23 @@ function createMap(earthquakes) {
     // Create the map object with options
     var map = L.map("map-id", {
       center: [40.73, -74.0059],
-      zoom: 12,
-      layers: [lightmap, earthquakes]
+      zoom: 12
+      // layers: [lightmap, earthquakes]
     });
   
     // Create a layer control, pass in the baseMaps and overlayMaps. Add the layer control to the map
-    L.control.layers(baseMaps, overlayMaps, {
-      collapsed: false
-    }).addTo(map);
-  }
+    // L.control.layers(baseMaps, overlayMaps, {
+    //   collapsed: false
+    // }).addTo(map);
+    console.log(earthquakes)
+
+    L.geoJson(earthquakes,{
+
+    }).addTo(map)
+}
   
-  function createMarkers(type) {
-  
+function createMarkers(type) {
+  console.log(type)
     var locations = type.metadata;
   
     // Initialize an array to hold earthquake markers
@@ -96,9 +106,9 @@ function createMap(earthquakes) {
       earthquakeMarkers.push(earthquakeMarkers);
     }
   
-    // Create a layer group made from the bike markers array, pass it into the createMap function
+    // Create a layer group made from the earthquake markers array, pass it into the createMap function
     createMap(L.layerGroup(earthquakeMarkers));
 }
 
 // Perform an API call to the USGS API to get station information. Call createMarkers when complete
-d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson", createMarkers);
+d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson", createMap);
